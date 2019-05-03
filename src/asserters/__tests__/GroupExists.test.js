ac95af0c7b9fbef4d853b8156f27e9fcb220aece
Ben Goldstein <beng@brownpapertickets.com>
2019-05-03 12:46:13 -0700

    First pass mocks

--- src/asserters/__tests__/GroupExists.test.js
+++ src/asserters/__tests__/GroupExists.test.js
@@ -1,4 +1,13 @@
 import { GroupExists } from "../GroupExists"
+const MOCK_FILE_INFO = {
+  existentGroup: () => true,
+  nonExistentGroup: () => false,
+}
+
+beforeEach(() => {
+  // Set up some mocked out file info before each test
+  require("../__mocks__/child_process").__setMockFiles(MOCK_FILE_INFO)
+})
 
 test("assert", async (done) => {
   const asserter = new GroupExists()
