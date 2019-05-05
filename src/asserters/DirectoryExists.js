7e2db35de9eb0950f9181124359de828185d47e6
John Lyon-Smith <john@lyon-smith.org>
2019-05-05 14:10:41 -0700

    Partial

--- src/asserters/DirectoryExists.js
+++ src/asserters/DirectoryExists.js
@@ -16,24 +16,19 @@ Example:
 class DirectoryExists {
   async assert(args) {
     try {
-      console.log(` assert is dir: ${args.path}`)
       return (await fs.lstat(args.path)).isDirectory()
     } catch (error) {
-      console.log(`assert failed`)
       return false
     }
   }
 
   async actualize(args) {
     const home = process.env.HOME
-    console.log(`Home: "${home}"`)
     try {
-      console.log(`mkdir ${args.path}`)
       // NOTE: probably should use ensureDir()
       await fs.mkdir(args.path)
       return true
     } catch (error) {
-      console.log(`Error creating dir: ${JSON.stringify(error)}`)
       return false
     }
   }
