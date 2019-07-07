4497184697d16964f368428f9573d3e66128f70e
John Lyon-Smith <john@lyon-smith.org>
2019-07-07 15:58:27 -0700

    Add @johnls/JSON5 to read script

--- src/TentacleTool.js
+++ src/TentacleTool.js
@@ -1,4 +1,6 @@
 import parseArgs from "minimist"
+import JSON5 from "@johnls/JSON5"
+import fs from "fs-extra"
 import { fullVersion } from "./version"
 import autobind from "autobind-decorator"
 
@@ -11,118 +13,9 @@ export class TentacleTool {
     this.debug = options.debug
   }
 
-  async processAssertions(ssh, username, password, assertScript) {
-    assertScript = this.expandAssertVariables(assertScript)
-    const vars = assertScript.vars || {}
-    const asserts = assertScript.assertions || []
-    let scriptSuccess = true
-    let an
-    let description
-    for (an = 0; an < asserts.length; an++) {
-      const assertionSpec = asserts[an]
-
-      const assertionName = assertionSpec.assert
-      description =
-        assertionSpec.description || `Step ${an + 1}: (${assertionName})`
-      const runAs = assertionSpec.runAs || ""
-
-      const args = assertionSpec.with || {}
-      const argsString = JSON.stringify(args)
-      const args64 = Buffer.from(argsString).toString("base64")
-      const sudo = runAs == "sudo" ? "sudo" : ""
-      const command = `${sudo} node doAssert.js ${assertionName} base64 ${args64}`
-      const assertSuccess = await this.runAssertion(
-        ssh,
-        username,
-        password,
-        command
-      )
-      if (!assertSuccess) {
-        scriptSuccess = false
-        break
-      }
-    }
-
-    if (scriptSuccess) {
-      this.log.info(chalk.green(">>> Script Completed Successfully"))
-    } else {
-      this.log.info(
-        chalk.red(`>>> Script Failed at step ${an} (${description})`)
-      )
-    }
-  }
-
-  async runAssertion(ssh, username, password, command) {
-    const result = await ssh.execCommand(command, {
-      options: { pty: !!password },
-      cwd: "/opt/octopus/asserters",
-      stdin: password + "\n",
-    })
-    const success = result.code == 0
-    const response = result.stdout
-    if (success) {
-      this.log.info(
-        chalk.green(`Assertion Success: ${success}\nResponse:\n${response}`)
-      )
-    } else {
-      this.log.info(
-        chalk.red(`Assertion Success: ${success}\nResponse:\n${response}`)
-      )
-    }
-
-    return success
-  }
-
-  expandAssertVariables(assertionScript) {
-    const varExpander = (tpl, args) =>
-      tpl.replace(/\${(\w+)}/g, (_, v) => args[v])
-    const variables = assertionScript.vars || {}
-    const assertions = assertionScript.assertions || {}
-    const assertionsString = JSON.stringify(assertions)
-    const expanded = varExpander(assertionsString, variables)
-    return { vars: variables, assertions: JSON.parse(expanded) }
-  }
-
-  getArgData(args) {
-    const encoding = args[1]
-    const encodedData = args[2]
-    let dataString = encodedData
-    if (encoding == "base64") {
-      dataString = Buffer.from(encodedData, "base64").toString("ascii")
-    }
-    dataString = this.expandEnvVariables(dataString)
-    return JSON.parse(dataString)
-  }
-
-  expandEnvVariables(dataString) {
-    const varExpander = (tpl, args) =>
-      tpl.replace(/{{(\w+)}}/g, (_, v) => args[v])
-    return varExpander(dataString, process.env)
-  }
-
-  async getAsserterInfo(asserterName) {
-    const registry = await this.getRegistry()
-    return registry[asserterName]
-  }
-
-  async getAsserter(asserterName) {
-    const info = await this.getAsserterInfo(asserterName)
-    // console.log(`getAsserterInfo for ${asserterName} : ${JSON.stringify(info)}`)
-    if (info) {
-      try {
-        const asserterClass = require(`./${info.module}`)
-        const container = {} // pass in logger at least
-        const asserter = new asserterClass(container)
-        return asserter
-      } catch (ex) {
-        console.log(`Error creating asserter ${info.module}: ${ex.message}`)
-      }
-    } else {
-      return null
-    }
-  }
-
   async run(argv) {
+    // TODO: Add a --server flag that starts an HTTP server and waits for requests
+
     const options = {
       boolean: ["help", "version", "debug"],
       string: [],
@@ -155,15 +48,26 @@ Options:
 
     const scriptFile = args._[0]
 
-    // TODO: Parse the script
-    // TODO: Add JSON5 source in directly and output line number information for issues
-    // TODO: Run the asserters in order, calling ensure if needed
-    // TODO: Ensure that errors in assert/ensure throw line number information
-    // TODO: Parse variable section of script
-    // TODO: Fill in variables is args
-    // TODO: Test out all the asserters without mocks
-    // TODO: Add a --server flag that starts an HTTP server and waits for requests
+    if (!scriptFile) {
+      throw new Error("Must supply an script file")
+    }
+
+    const script = JSON5.parse(await fs.readFile(scriptFile), {
+      wantNodes: true,
+    })
+
+    this.processScriptFile(script)
 
     return 0
+  }
+
+  processScriptFile(script) {
+    console.log(JSON5.stringify(script, null, "  "))
+    // TODO: Parse the options
+    // TODO: Parse the variables
+    // TODO: Parse the asserters
+    // TODO: Run the asserters in order, calling actualize if needed
+    // TODO: Fill in variables in args
+    // TODO: Ensure that errors in assert/actualize throw line number information
   }
 }
